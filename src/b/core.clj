bdf7671d19e2de9f68bcd7a57241fa0e4db439c3
Kanishk Kumar <kanishkkumar188nrt@gmail.com>
2019-07-27 01:28:16 +0530

    Transaction Complete

--- src/b/core.clj
+++ src/b/core.clj
@@ -47,24 +47,31 @@
 (defn refresh-account [ user account new-transactions]
   (let [updated-account (assoc account :transactions new-transactions)]
     (let [account-index (.indexOf (:accounts user) account)]
-      (assoc (db) (.indexOf (db) user) (merge user (:accounts (assoc (:accounts user) account-index updated-account)))))))
+      (assoc (db) (.indexOf (db) user) (merge user (hash-map :accounts (assoc (:accounts user) account-index updated-account)))))))
 
 (defn write-in-db [data]
-  (print data "aaasdasdads")
-  (spit "src/accounts.json" (json/write-str data)))
+  (print "write data" data)
+  (spit "resources/users.json" (json/write-str data)))
 
-(defn transact [inputs action]
+(defn credit-with-last-balance [account amount]
+  (credit (last-transaction (transactions account)) amount))
+
+(defn debit-with-last-balance [account amount]
+  (debit (last-transaction (transactions account)) amount))
+
+(defn calculate-transaction [valid-user new-balance action amount]
+  (let [new-transaction (new-transaction amount action new-balance)]
+    (let [merge-transactions (merge-transactions (transactions (saving-account valid-user)) new-transaction)]
+      (let [updated-user-account (refresh-account valid-user (saving-account valid-user) merge-transactions)]
+        (write-in-db updated-user-account)))))
+
+(defn transact [inputs params]
   (let [valid-user (is-valid-account inputs)]
-    (case (:action action)
-      "credit-in-saving" (do
-                           (let [new-balance (credit (last-transaction (transactions (saving-account valid-user))) (:amount action))]
-                             (let [new-transaction (new-transaction (:amount action) "credit" new-balance)]
-                               (let [merge-transactions (merge-transactions (transactions (saving-account valid-user)) new-transaction)]
-                                 (let [updated-user-account (refresh-account valid-user (saving-account valid-user) merge-transactions)]
-                                   (write-in-db updated-user-account))))))
-      "debit-in-saving" (debit  (last-transaction (saving-account valid-user)) (:amount action))
-      "credit-in-current" (credit (last-transaction (current-account valid-user)) (:amount action))
-      "debit-in-current" (debit (last-transaction (current-account valid-user)) (:amount action)))))
+    (case (:action params)
+      "credit-in-saving" (calculate-transaction valid-user (credit-with-last-balance (saving-account valid-user) (:amount params)) "credit" (:amount params))
+      "debit-in-saving" (calculate-transaction valid-user (debit-with-last-balance (saving-account valid-user) (:amount params)) "debit" (:amount params))
+      "credit-in-current" (calculate-transaction valid-user (credit-with-last-balance (current-account valid-user) (:amount params)) "credit" (:amount params))
+      "debit-in-current" (calculate-transaction valid-user (debit-with-last-balance (current-account valid-user) (:amount params)) "debit" (:amount params)))))
 
 
 (defn login [inputs]
@@ -80,11 +87,11 @@
               )))
 
 (defn register-account [account]
-  (spit "src/accounts.json" (json/write-str (prepare-registration-attributes account))))
+  (spit "resources/users.json" (json/write-str (prepare-registration-attributes account))))
 
 (login {:username "kanishk" :password "123"})
 (register-account {:username "kanishk" :password "123"})
-(transact {:username "kanishk" :password "123"} {:action "credit-in-saving" :amount 12})
+(transact {:username "kanishk" :password "123"} {:action "debit-in-saving" :amount 20})
 
 (conj {:a "b" :c [{:a "b"}]} {:c [{:b "x"}]})
 (assoc ["a" "b" "c"] 0 2)
